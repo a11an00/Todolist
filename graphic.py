d0b7f231de2a2ca11f56cf991cbd844c660c388b
Allan Kiptanui <akiptanui17@gmail.com>
2026-02-03 23:33:09 +0300

    gui LEARN

---
+++ graphic.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root=Tk()
+# Create a label widget with the text "Hello, Allan!"
+myLabel = Label(root, text="Hello, Allan!")
+nextLabel=Label(root, text="TO DO LIST!")
+# Place the label in the window
+myLabel.pack()
+nextLabel.pack()  
+
+root.mainloop()
